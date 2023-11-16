4dc6f418f2b5a9d8e730ed1c85f20d6eb3ff3401
Vasilis Gionis <56933592+vasilisgionis@users.noreply.github.com>
2023-11-17 01:24:46 +0200

    Update list.java

--- list.java
+++ list.java
@@ -78,41 +78,93 @@ class List {
 		return filterlist;
 	}
 
-	public void mergeWith(List list) {
-		List mergedlist, copiedthis, copiedlist = new List();
-		copiedthis=this.copy(); copiedlist=list.copy();
-		copiedthis.print(); copiedlist.print();
-		Node current = this.head.next;
-		
-
+	public List mergeWith(List list) {
+		List copiedthis, copiedlist = new List();
+		copiedthis=this.copy(); copiedlist=list.copy(); //αντιγραφω τις 2 λιστες για να συγκρινω τα nodes τους και αν τα βαλω σε αυξουσα σειρα σε μια νεα λιστα με ονομα mergedlist
+		insertionSort(copiedlist); //επειτα χρησιμοποιω την insertionSort που υλοποιησα ως μεθοδο για να ταξινομησω τα αντιγραφα των λιστων σε αυξουσα σειρα για να κανω την συγκριση πιο ευκολη 
+		insertionSort(copiedthis);
+		copiedthis.print();
+		copiedlist.print();
+		List mergedlist = new List();
+		Node current = copiedthis.head.next;
+		Node currentlist = copiedlist.head.next;
+		while (current != null && currentlist!=null){
+			if (current.key() <= currentlist.key()){ //συγκρινω τα nodes μεταξυ τους 
+				
+				mergedlist.insertFirst(current.key()); //εισχωρω στην mergedlist το κλειδι του current αν ο current ειναι οντως μικροτερος
+				current.prev.next=current.next;
+				if (current.next != null){ //αλλαζουμε το pointer του επομενου απο τον προηγουμενο selected node να ειναι ο επόμενος node απο τον selected (selected = current)
+					current.next.prev=current.prev;
+				}
+			
+				current=current.next;
+			}else{
+				
+				mergedlist.insertFirst(currentlist.key());
+				currentlist.prev.next=currentlist.next;
+				if (currentlist.next!=null){
+					currentlist.next.prev=currentlist.prev;
+				}
+				
+				currentlist=currentlist.next;
+			}
+		}
+		while(current!=null){
+			mergedlist.insertFirst(current.key()); //εισχωρω στην mergedlist το κλειδι του current αν ο current ειναι οντως μικροτερος
+				current.prev.next=current.next;
+				if (current.next != null){ //αλλαζουμε το pointer του επομενου απο τον προηγουμενο selected node να ειναι ο επόμενος node απο τον selected (selected = current)
+					current.next.prev=current.prev;
+				}
+				
+				current=current.next;
+			
+		}
+		while(currentlist!=null){
+			mergedlist.insertFirst(currentlist.key());
+				currentlist.prev.next=currentlist.next;
+				if (currentlist.next!=null){
+					currentlist.next.prev=currentlist.prev;
+				}
+				
+				currentlist=currentlist.next;
+		}
+		insertionSort(mergedlist);
+		return mergedlist;
 	}
 
-	//public List largest(int k) {
-		/* your code here */
-	//}
-
-	public static void insertionSort(List list) {
-		Node current = list.head.next;
-		int length=0;
-		while (current!=list.tail){
-			length++;
-			current.print("  " ,"  ");
-			current=current.next;
+	public List largest(int k) {
+		List largest = new List();
+		Node current = this.head.next;
+		while(current!=null){
+			if (current.key() >= k){
+				largest.insertFirst(current.key());
+				current=current.next;
+			}else{
+				current=current.next;
+			}
 		}
-			System.out.println(length);
-			
+		return largest;
+	}
+
+	public static void insertionSort(List list) {	
 		Node currentInsertion = list.head.next;
-		for (int index=1; index<=length; index++){ //με αυτην την λουπα θα κανω οσες συγκρισεις οσα ειναι και τα nodes της λιστας για να ταξινομηθουν ολα 
+		while(currentInsertion!=list.tail){ //με αυτην την λουπα θα κανω οσες συγκρισεις οσα ειναι και τα nodes της λιστας για να ταξινομηθουν ολα 
 			Node temp = currentInsertion;
 			Node checking = currentInsertion.prev; //φτιαχνω αυτο το node για συγκρισεις με τα προηγουμενα node keys απο αυτον που συγκρινω
-			while(checking!=list.head.prev && currentInsertion.key() <= checking.key()){  //συγκρινω αμα το παρων node ειναι μικροτερο απο το πορηγουμενο του 
-				checking=currentInsertion;
-				checking=temp;//κανω τις απαραιτητες πραξεις για να ολισθησω την λιστα καθε φορα που ο κομβος που ελεγχω ειναι μικροτερος απο τον προηγμουμενο 
-					
+			while(checking!=list.head && currentInsertion!=null && temp.key() < checking.key()){  //συγκρινω αμα το παρων node ειναι μικροτερο απο το πορηγουμενο του 
+				temp.prev.next=temp.next;
+				if (temp.next != null) {
+					temp.next.prev = temp.prev;
+				}
+				temp.prev=checking.prev;
+				checking.prev.next=temp;
+				temp.next=checking;
+				checking.prev=temp;
+				checking=temp.prev;//κανω τις απαραιτητες πραξεις για να ολισθησω την λιστα καθε φορα που ο κομβος που ελεγχω ειναι μικροτερος απο τον προηγμουμενο 
+				
 			}
-			checking.next.key(temp.key());
+				currentInsertion=currentInsertion.next;
 		}
-		/* your code here */
 	}
 
 	public static void main(String[] args) {
@@ -123,13 +175,13 @@ class List {
 
 	        // Insert elements into the list
 	       for (int i=0; i<=4; i++) {
-			int random = rand.nextInt(10);
+			int random = rand.nextInt(15);
 	    	   list.insertFirst(random);
 	       }
 	       
 	       //Insert elements into the list2
-	       for (int i=0; i<=6; i++) {
-				int random = rand.nextInt(10);
+	       for (int i=0; i<=4; i++) {
+				int random = rand.nextInt(15);
 		    	   list2.insertFirst(random);
 		       }
 	  
@@ -144,7 +196,15 @@ class List {
 	        fl=list.filter(list2);
 	        fl.print();
 
+			System.out.println("Insertion Sorted list:");
 			insertionSort(list);
-	        
-	}
+			list.print();
+
+			System.out.println("The merged list one;");
+			list.mergeWith(list2).print();
+
+			System.out.println("The list with the bigger k elements is :");
+			list.print();
+			list.largest(5).print();
+			}
 }
